377dcf59ad44a052cc2afa742718e451ce8c07fd
aya uezato <a.u.91leo@gmail.com>
2019-06-02 16:05:23 +0900

    ABOUT USのページ作成

---
+++ 00/js/app.js
@@ -0,0 +1,25 @@
+let password =true;
+let value = $('.form-check-input').val();
+
+$(function (){
+    $('.fa-plane-departurebtn').on('click', function(){
+      
+       if(value == password) {
+
+        console.log('3');
+        $('.fa-plane-departure').fadeIn( function(){
+            console.log('5')
+            $('.fas').animate({
+                'top': '-50px',
+                'left': '1200px',
+            },10000 , function(){
+
+            });
+        })
+
+       } else {
+           alert('パスワードが間違っております。もう一度入力してください。')
+       }
+       
+    })
+})
--- js/app.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $('.btn').on('click', function() {
-        $('.fa-plane-departure').animate({
-            'marginLeft':'500px'
-        },10000);
-    })
-})
